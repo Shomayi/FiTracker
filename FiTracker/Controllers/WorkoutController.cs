2bce0cc613b63dda6766c7d5c4a70f0b85effeb3
Teun Arts <teunarts2@gmail.com>
2025-11-10 13:09:55 +0100

    added workoutcontroller and view+navigation

---
+++ FiTracker/Controllers/WorkoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiTracker.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        public IActionResult Workouts()
+        {
+            return View();
+        }
+    }
+}
